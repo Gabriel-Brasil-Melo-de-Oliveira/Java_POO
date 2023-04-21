7bfabb30589a9e5ece0dd9f0540f6895f967a7d4
Gabriel Brasil <uece.gabrielbmo@gmail.com>
2023-04-21 19:07:20 -0300

    att

--- polimorfismoDesafio/Main.java
+++ polimorfismoDesafio/Main.java
@@ -1,19 +1,35 @@
 public class Main {
     public static void main(String[] args){
-        Professor professor1 = new Professor("Gabriel",12,"mestre",10,true);
-        System.out.println(professor1);
+       /*  
+        
         professor1.calcularSalario();
         System.out.println("\nDepois de calcular o salario: \n");
         System.out.println(professor1);
-        Secretario secretario1 = new Secretario("Jose", 10, 40);
+        
         System.out.println(secretario1);
         System.out.println("\nDepois de calcular o salario\n");
         secretario1.calcularSalario();
         System.out.println(secretario1);
 
-        Servidor servidor1 = new Servidor("Igor", 0, 10);
+        
         System.out.println(servidor1);
         servidor1.calcularSalario();
-        System.out.println(servidor1);
-    }
+        System.out.println(servidor1);*/
+        Professor professor1 = new Professor("Gabriel",12,"mestre",10,true);
+        Secretario secretario1 = new Secretario("Jose", 10, 40);
+        Servidor servidor1 = new Servidor("Igor", 0, 10);
+
+        Universidade uece = new Universidade("UECE");
+        uece.adicionarFuncionario(professor1);
+        uece.adicionarFuncionario(secretario1);
+        uece.adicionarFuncionario(servidor1);
+
+        for(int i = 0; i < uece.getLista().size(); i++){
+            uece.getLista().get(i).calcularSalario();
+        }
+        
+
+        uece.folhaDePagamento();
+
+    } 
 }
--- polimorfismoDesafio/Universidade.java
+++ polimorfismoDesafio/Universidade.java
@@ -14,4 +14,18 @@ public class Universidade {
         this.nome =  nome;
         listaFuncionario = new ArrayList<Funcionario>();
     }
+
+    public void adicionarFuncionario(Funcionario pessoa){
+        this.listaFuncionario.add(pessoa);
+    }
+
+    public void folhaDePagamento(){
+        double valorFinal = 0;
+        System.out.println("\nUniverdade: " + this.nome + "\n\n\n\n\n");
+        for(Funcionario pessoa: listaFuncionario){
+            System.out.println(pessoa);
+            valorFinal += pessoa.getSalarioMinimo();
+        }
+        System.out.println("\nVALOR FINAL PAGO: " + valorFinal);
+    }
 }
